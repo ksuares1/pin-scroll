83062a2293616471663526a4c13850d8d91147a2
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-06-26 22:50:06 -0400

    working on Home component

---
+++ infinite-scroll/pin-scroll/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import showBasic from '../components/Navbar.js';
+import showSetBasic from '../components/Navbar.js';
+
+
+const HomeComponent =() => {
+    return(
+        <div>
+            <showBasic/>
+            <showSetBasic/>
+        </div>
+    )
+}
+export default HomeComponent;
