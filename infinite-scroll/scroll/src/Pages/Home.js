27d8d083d25507b68957a5f6b7d1a6f5e92bec09
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-06-27 18:41:06 -0400

    added navbar

---
+++ infinite-scroll/scroll/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar.js';
+
+const HomeComponent = () => {
+    return (
+        <main>
+      
+            
+        </main>
+    )
+}
+
+export default HomeComponent;
