17085d7be22e8888414b4940eb18c1ccea1cbe45
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-06-27 18:21:39 -0400

    Added font awesome and google font

--- infinite-scroll/scroll/src/App.js
+++ infinite-scroll/scroll/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
 
 function App() {
   return (
