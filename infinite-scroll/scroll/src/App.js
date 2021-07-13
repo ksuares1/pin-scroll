f4d39513b3a31475a53d95294aa8737a16d786b0
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-07-13 19:06:32 -0400

    added header file

--- infinite-scroll/scroll/src/App.js
+++ infinite-scroll/scroll/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Pinbox from './components/Pinbox';
+import Header from './components/Header';
 import './App.css';
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
       <br>
       </br>
       <Pinbox/>
--- infinite-scroll/scroll/src/components/Header.js
+++ infinite-scroll/scroll/src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
     return (
        <header>
            <h1> Task Tracker</h1>
+           <h2> Welcome</h2>
        </header>
     )
 }
